8f2a698149ade6d053050567a2f40a14623f69be
amzesmoro <amzesmoro05@gmail.com>
2020-07-04 16:50:17 +0700

    update week04

--- Week04/Blockbuster/app/src/main/java/com/amzesmoro/blockbuster/data/Movie.kt
+++ Week04/Blockbuster/app/src/main/java/com/amzesmoro/blockbuster/data/Movie.kt
@@ -2,7 +2,10 @@ package com.amzesmoro.blockbuster.data
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parceler
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Movie(
     val id: Int?,
     val releaseDate: String?,
@@ -28,30 +31,23 @@ data class Movie(
         parcel.readString()
     )
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeString(releaseDate)
-        parcel.writeString(title)
-        parcel.writeString(summary)
-        parcel.writeValue(poster)
-        parcel.writeValue(backdropPoster)
-        parcel.writeString(genre)
-        parcel.writeString(movieDuration)
-        parcel.writeString(originalLanguage)
-        parcel.writeString(rating)
-    }
+    companion object : Parceler<Movie> {
 
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
+        override fun Movie.write(parcel: Parcel, flags: Int) {
+            parcel.writeValue(id)
+            parcel.writeString(releaseDate)
+            parcel.writeString(title)
+            parcel.writeString(summary)
+            parcel.writeValue(poster)
+            parcel.writeValue(backdropPoster)
+            parcel.writeString(genre)
+            parcel.writeString(movieDuration)
+            parcel.writeString(originalLanguage)
+            parcel.writeString(rating)
         }
 
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
+        override fun create(parcel: Parcel): Movie {
+            return Movie(parcel)
         }
     }
 }
--- Week05/Blockbuster/app/src/main/java/com/amzesmoro/blockbuster/ui/movie/MainActivity.kt
+++ Week05/Blockbuster/app/src/main/java/com/amzesmoro/blockbuster/ui/movie/MainActivity.kt
@@ -1,28 +1,31 @@
-package com.amzesmoro.blockbuster.ui.movie
+package com.amzesmoro.blockbuster.ui.login
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.amzesmoro.blockbuster.R
 import com.amzesmoro.blockbuster.model.User
 import com.amzesmoro.blockbuster.model.UserPreference
-import com.amzesmoro.blockbuster.ui.login.MainActivity
+import com.amzesmoro.blockbuster.ui.movie.MovieActivity
+import com.amzesmoro.blockbuster.ui.movie.MoviePagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.tabLayout
+import kotlinx.android.synthetic.main.activity_main.viewPager
 import kotlinx.android.synthetic.main.activity_movie.*
 
-class MovieActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var user: User
     private lateinit var userPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
+        setContentView(R.layout.activity_main)
         supportActionBar?.elevation = 0.0f
-        val pagerAdapter = MoviePagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
+        val pagerAdapter = MoviePagerAdapter(supportFragmentManager, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
         viewPager.adapter = pagerAdapter
         tabLayout.setupWithViewPager(viewPager)
         userPreference = UserPreference(this)
@@ -41,4 +44,5 @@ class MovieActivity : AppCompatActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         return super.onOptionsItemSelected(item)
     }
+
 }
